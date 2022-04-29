69a0ea1ce51f168bf4baa3dc18138be766e32bb8
GetRektd <97607314+GetRektd@users.noreply.github.com>
2022-04-29 09:03:45 -0400

    Initial commit

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D bird;
+
+    int score = 0;
+
+    public Text scoreUI;
+
+    
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bird = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            bird.AddForce(new Vector2(0,1) * 200);
+        }
+
+       
+
+
+        
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+
+    {
+        if(other.gameObject.CompareTag("Points"))
+        {
+            score = score + 1;
+
+            scoreUI.text = score.ToString();
+
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Bacon"))
+        {
+            Destroy(other.gameObject);
+
+            Pipe pipeScript =  GameObject.Find("Pipes").GetComponent<Pipe>();
+
+            pipeScript.speed = 10;
+
+            Debug.Log(pipeScript.speed);
+
+        }
+
+    }
+
+
+
+
+
+}
